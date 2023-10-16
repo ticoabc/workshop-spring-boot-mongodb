6844c6c8dd4ee6383854518e825fe069e2918379
Tiago de Freitas <tiagoabc@gmail.com>
2023-10-16 08:03:37 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/project/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.project.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.workshopmongo.domain.Post;
+import com.project.workshopmongo.domain.User;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
